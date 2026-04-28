a61854426319de7ba21e6d9d44e13cfaa2ff98a3
Braden <bjz-github@spwiz.com>
2026-04-28 14:56:50 -0600

    Improved launch file

--- launch/sonar3d.launch.py
+++ launch/sonar3d.launch.py
@@ -1,16 +1,29 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+
 
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'ip',
+            default_value='192.168.194.96',
+            description='IP address of the Sonar 3D-15'
+        ),
+        DeclareLaunchArgument(
+            'speed_of_sound',
+            default_value='1491',
+            description='Speed of sound in m/s'
+        ),
         Node(
             package='sonar3d',
             executable='sonar_publisher',
             name='sonar_node',
             output='screen',
-            parameters=[
-                {'IP': '192.168.194.96'},  # Change to your sonar IP, '192.168.194.96' is the fallback ip.
-                {'speed_of_sound': 1491}
-            ]
+            parameters=[{
+                'IP': LaunchConfiguration('ip'),
+                'speed_of_sound': LaunchConfiguration('speed_of_sound'),
+            }]
         )
     ])
